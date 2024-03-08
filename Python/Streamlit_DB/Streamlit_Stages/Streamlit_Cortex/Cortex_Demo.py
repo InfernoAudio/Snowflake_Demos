4d80062d6ccd6c3ec95547b91d90a1232ef41dd0
Eric Heilman <infernoaudio@gmail.com>
2024-03-08 06:34:10 -0500

    Added Summarize

--- Python/Streamlit_DB/Streamlit_Stages/Streamlit_Cortex/Cortex_Demo.py
+++ Python/Streamlit_DB/Streamlit_Stages/Streamlit_Cortex/Cortex_Demo.py
@@ -33,43 +33,18 @@ def open_session():
 session = open_session()
 
 #CREATE LANGUAGE DICT
-lang_code = {
-   "languages":{
-      "English":{
-         "code":"en"         
-      },
-      "French":{
-         "code":"fr"         
-      },
-      "German":{
-         "code":"de"         
-      },
-      "Italian":{
-         "code":"it"         
-      },
-      "Japanese":{
-         "code":"ja"         
-      },
-      "Korean":{
-         "code":"ko"         
-      },
-      "Polish":{
-         "code":"pl"         
-      },
-      "Portuguese":{
-         "code":"pt"         
-      },
-      "Russian":{
-         "code":"ru"         
-      },
-      "Spanish":{
-         "code":"es"         
-      },
-      "Swedish":{
-         "code":"sv"         
-      }
-   }
-}
+lang_code = {"English":"en",
+             "French":"fr",
+             "German":"de",
+             "Italian":"it",
+             "Japanese":"ja",
+             "Korean":"ko",
+             "Polish":"pl",
+             "Portuguese":"pt",
+             "Russian":"ru",
+             "Spanish":"es",
+             "Swedish":"sv"
+            }
 
 #CREATE LLM OPTIONS
 llm_models = ["llama2-70b-chat",
@@ -80,8 +55,9 @@ llm_models = ["llama2-70b-chat",
 
 #CORTEX TRANSLATE FUNCTION
 def cortex_translate(src_text,src_lang,tgt_lang):
-    src_lang_cd = lang_code["languages"][src_lang]["code"]
-    tgt_lang_cd = lang_code["languages"][tgt_lang]["code"]
+    src_text = src_text.replace("'","\\'")
+    src_lang_cd = lang_code[src_lang]
+    tgt_lang_cd = lang_code[tgt_lang]
     trans_sql = f"""
     SELECT SNOWFLAKE.CORTEX.TRANSLATE('{src_text}','{src_lang_cd}','{tgt_lang_cd}')
     """
@@ -90,6 +66,7 @@ def cortex_translate(src_text,src_lang,tgt_lang):
     return tgt_text
 
 def cortex_complete(model_name,prompt):
+    prompt = prompt.replace("'","\\'")
     comp_sql = f"""
     SELECT SNOWFLAKE.CORTEX.COMPLETE('{model_name}','{prompt}')    
     """
@@ -97,29 +74,44 @@ def cortex_complete(model_name,prompt):
 
     return comp_text
 
+def cortex_summarize(text):
+    text = text.replace("'","\\'")
+    summ_sql = f"""
+    SELECT SNOWFLAKE.CORTEX.SUMMARIZE('{text}') LIMIT 1
+    """
+    summ_txt = session.sql(summ_sql).collect()[0][0]
+
+    return summ_txt
+
 #START APP
 st.header("Snowflake Cortex Demo")
 
 #SET TABS
-tbTranslate,tbComplete = st.tabs(["Translate","Complete"])
+tbTranslate,tbComplete,tbSummarize = st.tabs(["Translate","Complete","Summarize"])
 #BUILD UI
 with tbTranslate:    
     colLang = st.columns(2)
     with colLang[0]:
-        src_lang = st.selectbox(label="Choose Source Language",options=lang_code["languages"])
+        src_lang = st.selectbox(label="Choose Source Language",options=dict(sorted(lang_code.items())))
     with colLang[1]:
-        tgt_lang = st.selectbox(label="Choose Target Language",options=lang_code["languages"])
+        tgt_lang = st.selectbox(label="Choose Target Language",options=dict(sorted(lang_code.items())))
     colTrans = st.columns(2)
     with colTrans[0]:
         src_text = st.text_area(label="Enter Source Text")
-    with colTrans[1]:
-        tgt_text = cortex_translate(src_text,src_lang,tgt_lang)
+    with colTrans[1]:        
+        tgt_text = cortex_translate(src_text,src_lang,tgt_lang)        
         st.text_area(label="Translation",value=tgt_text)
 
 #COMPLETE
 with tbComplete:
     llm_name = st.selectbox(label="Choose an LLM",options=llm_models)
-    comp_question = st.text_input(label="Enter your chat prompt:",value="Hello there! What is your name?")
-    comp_answer = cortex_complete(llm_name,comp_question)
-    st.write("Snowflake Response:")
-    st.write(comp_answer)
+    comp_question = st.text_input(label="Enter your chat prompt:")
+    if comp_question:
+        comp_answer = cortex_complete(llm_name,comp_question)
+        st.write("Snowflake Response:")
+        st.write(comp_answer)
+#SUMMARIZE
+with tbSummarize:
+    txt_to_summ = st.text_area(label="Enter text to Summarize:")
+    if txt_to_summ:
+        st.write(cortex_summarize(txt_to_summ))
